78a947c9938c39710f9948d8e75d99537e53488f
Matthew Krueger <contact@matthewkrueger.com>
2021-02-23 16:25:25 -0500

    Removed RenderAPI from Utils.hpp
    Signed-off-by: Matthew Krueger <contact@matthewkrueger.com>

--- Source/GEOGL/Modules/Utils/include/GEOGL/Utils.hpp
+++ Source/GEOGL/Modules/Utils/include/GEOGL/Utils.hpp
@@ -31,7 +31,6 @@
 #include "../../Headers/Dependencies.hpp"
 
 #include "../../Logging/PublicLog.hpp"
-#include "../../../../Rendering/RendererAPI.hpp"
 #include "../../Callbacks.hpp"
 #include "../../InputCodes.hpp"
 #include "../../InputCodesConverter.hpp"
