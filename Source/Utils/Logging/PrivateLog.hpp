4dfe92004a401f43523766fe324c097f225bfae4
Matthew Krueger <contact@matthewkrueger.com>
2021-05-12 14:15:47 -0400

    Added Line and File logging
    Signed-off-by: Matthew Krueger <contact@matthewkrueger.com>

--- Source/Utils/Logging/PrivateLog.hpp
+++ Source/Utils/Logging/PrivateLog.hpp
@@ -33,30 +33,30 @@
 //#   define GEOGL_CORE_TRACE
 #   define GEOGL_CORE_INFO
 #   define GEOGL_CORE_WARN
-#   define GEOGL_CORE_ERROR(...)            ::GEOGL::Log::getCoreLogger()->error(__VA_ARGS__)
-#   define GEOGL_CORE_CRITICAL(...)         ::GEOGL::Log::getCoreLogger()->critical(__VA_ARGS__)
+#   define GEOGL_CORE_ERROR(...)            ::GEOGL::Log::getCoreLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CORE_CRITICAL(...)         ::GEOGL::Log::getCoreLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 #else
-#   define GEOGL_CORE_TRACE(...)          ::GEOGL::Log::getCoreLogger()->trace(__VA_ARGS__)
-#   define GEOGL_CORE_INFO(...)             ::GEOGL::Log::getCoreLogger()->info(__VA_ARGS__)
-#   define GEOGL_CORE_WARN(...)             ::GEOGL::Log::getCoreLogger()->warn(__VA_ARGS__)
-#   define GEOGL_CORE_ERROR(...)            ::GEOGL::Log::getCoreLogger()->error(__VA_ARGS__)
-#   define GEOGL_CORE_CRITICAL(...)         ::GEOGL::Log::getCoreLogger()->critical(__VA_ARGS__)
+#   define GEOGL_CORE_TRACE(...)          ::GEOGL::Log::getCoreLogger()->trace(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CORE_INFO(...)             ::GEOGL::Log::getCoreLogger()->info(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CORE_WARN(...)             ::GEOGL::Log::getCoreLogger()->warn(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CORE_ERROR(...)            ::GEOGL::Log::getCoreLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CORE_CRITICAL(...)         ::GEOGL::Log::getCoreLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 #endif
-//#define GEOGL_CORE_TRACE_NOSTRIP(...)     ::GEOGL::Log::getCoreLogger()->trace(__VA_ARGS__)
-#define GEOGL_CORE_INFO_NOSTRIP(...)        ::GEOGL::Log::getCoreLogger()->info(__VA_ARGS__)
-#define GEOGL_CORE_WARN_NOSTRIP(...)        ::GEOGL::Log::getCoreLogger()->warn(__VA_ARGS__)
-#define GEOGL_CORE_ERROR_NOSTRIP(...)       ::GEOGL::Log::getCoreLogger()->error(__VA_ARGS__)
-#define GEOGL_CORE_CRITICAL_NOSTRIP(...)    ::GEOGL::Log::getCoreLogger()->critical(__VA_ARGS__)
+//#define GEOGL_CORE_TRACE_NOSTRIP(...)     ::GEOGL::Log::getCoreLogger()->trace(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_CORE_INFO_NOSTRIP(...)        ::GEOGL::Log::getCoreLogger()->info(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_CORE_WARN_NOSTRIP(...)        ::GEOGL::Log::getCoreLogger()->warn(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_CORE_ERROR_NOSTRIP(...)       ::GEOGL::Log::getCoreLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_CORE_CRITICAL_NOSTRIP(...)    ::GEOGL::Log::getCoreLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 
 #ifdef GEOGL_ENABLE_ASSERTS
-#   define GEOGL_CORE_ASSERT(x, ...) { if(!(x)) { GEOGL_CORE_ERROR_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__)); GEOGL_DEBUG_BREAK(); } } (void(0))
+#   define GEOGL_CORE_ASSERT(x, ...) { if(!(x)) { GEOGL_CORE_ERROR_NOSTRIP(GEOGL_PREPARE_LOG_ENTRY_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__))); GEOGL_DEBUG_BREAK(); } } (void(0))
 #else
 #   define GEOGL_CORE_ASSERT(x, ...) {x;} (void(0))
 #endif
 /**
  * Forms a non-stripping assert. If not launched with a debugger, this causes a hard crash
  */
-#define GEOGL_CORE_ASSERT_NOSTRIP(x, ...) { if(!(x)) { GEOGL_CORE_ERROR_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__)); GEOGL_DEBUG_BREAK(); } } (void(0))
+#define GEOGL_CORE_ASSERT_NOSTRIP(x, ...) { if(!(x)) { GEOGL_CORE_ERROR_NOSTRIP(GEOGL_PREPARE_LOG_ENTRY_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__))); GEOGL_DEBUG_BREAK(); } } (void(0))
 
 
 #endif //GEOGL_ENGINE_PRIVATELOG_HPP
--- Source/Utils/Logging/PublicLog.hpp
+++ Source/Utils/Logging/PublicLog.hpp
@@ -31,6 +31,25 @@
 #include "../Headers/Refs.hpp"
 #include <spdlog/spdlog.h>
 
+#define GEOGL_FORMAT(...)           ::fmt::format(__VA_ARGS__)
+
+/* Define Logging Behavior. To change, set GEOGL_LOG_FILE to 1 to log file info, or zero to not */
+#ifndef GEOGL_LOG_FILE
+#   ifdef NDEBUG
+#       define GEOGL_LOG_FILE 0
+#   else
+#       define GEOGL_LOG_FILE 1
+#   endif
+#endif
+
+#if GEOGL_LOG_FILE == 0
+#   define GEOGL_PREPARE_LOG_ENTRY(...)         __VA_ARGS__
+#else
+#   define GEOGL_PREPARE_LOG_ENTRY(...)         "{}\nIn file {}:{}", GEOGL_FORMAT(__VA_ARGS__), __FILE__, __LINE__
+#endif
+#   define GEOGL_PREPARE_LOG_ENTRY_NOSTRIP(...)         "{}\nIn file {}:{}\n", GEOGL_FORMAT(__VA_ARGS__), __FILE__, __LINE__
+
+
 namespace GEOGL{
 
 
@@ -53,39 +72,38 @@ namespace GEOGL{
 }
 
 // client log macros
-#define GEOGL_FORMAT(...)           ::fmt::format(__VA_ARGS__)
 #ifdef NDEBUG
 //#   define GEOGL_TRACE
 #   define GEOGL_INFO
 #   define GEOGL_WARN
-#   define GEOGL_ERROR(...)         ::GEOGL::Log::getClientLogger()->error(__VA_ARGS__)
-#   define GEOGL_CRITICAL(...)      ::GEOGL::Log::getClientLogger()->critical(__VA_ARGS__)
+#   define GEOGL_ERROR(...)         ::GEOGL::Log::getClientLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CRITICAL(...)      ::GEOGL::Log::getClientLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 #else
 /**
  * Enables asserts when compiling in debug mode
  */
 #   define GEOGL_ENABLE_ASSERTS
-#   define GEOGL_TRACE(...)       ::GEOGL::Log::getClientLogger()->trace(__VA_ARGS__)
-#   define GEOGL_INFO(...)          ::GEOGL::Log::getClientLogger()->info(__VA_ARGS__)
-#   define GEOGL_WARN(...)          ::GEOGL::Log::getClientLogger()->warn(__VA_ARGS__)
-#   define GEOGL_ERROR(...)         ::GEOGL::Log::getClientLogger()->error(__VA_ARGS__)
-#   define GEOGL_CRITICAL(...)      ::GEOGL::Log::getClientLogger()->critical(__VA_ARGS__)
+#   define GEOGL_TRACE(...)       ::GEOGL::Log::getClientLogger()->trace(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_INFO(...)          ::GEOGL::Log::getClientLogger()->info(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_WARN(...)          ::GEOGL::Log::getClientLogger()->warn(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_ERROR(...)         ::GEOGL::Log::getClientLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#   define GEOGL_CRITICAL(...)      ::GEOGL::Log::getClientLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 #endif
 
-//#define GEOGL_TRACE_NOSTRIP(...)  ::GEOGL::Log::getClientLogger()->trace(__VA_ARGS__)
-#define GEOGL_INFO_NOSTRIP(...)     ::GEOGL::Log::getClientLogger()->info(__VA_ARGS__)
-#define GEOGL_WARN_NOSTRIP(...)     ::GEOGL::Log::getClientLogger()->warn(__VA_ARGS__)
-#define GEOGL_ERROR_NOSTRIP(...)    ::GEOGL::Log::getClientLogger()->error(__VA_ARGS__)
-#define GEOGL_FATAL_CRITICAL(...)   ::GEOGL::Log::getClientLogger()->critical(__VA_ARGS__)
+//#define GEOGL_TRACE_NOSTRIP(...)  ::GEOGL::Log::getClientLogger()->trace(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_INFO_NOSTRIP(...)     ::GEOGL::Log::getClientLogger()->info(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_WARN_NOSTRIP(...)     ::GEOGL::Log::getClientLogger()->warn(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_ERROR_NOSTRIP(...)    ::GEOGL::Log::getClientLogger()->error(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
+#define GEOGL_FATAL_CRITICAL(...)   ::GEOGL::Log::getClientLogger()->critical(GEOGL_PREPARE_LOG_ENTRY(__VA_ARGS__))
 
 #ifdef GEOGL_ENABLE_ASSERTS
-#   define GEOGL_ASSERT(x, ...) { if(!(x)) { GEOGL_ERROR_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__)); GEOGL_DEBUG_BREAK(); } } (void(0))
+#   define GEOGL_ASSERT(x, ...) { if(!(x)) { GEOGL_ERROR_NOSTRIP(GEOGL_PREPARE_LOG_ENTRY_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__))); GEOGL_DEBUG_BREAK(); } } (void(0))
 #else
 #   define GEOGL_ASSERT(x, ...) { x; } (void(0))
 #endif
 /**
  * Forms a non-stripping assert. If not launched with a debugger, this causes a hard crash
  */
-#define GEOGL_ASSERT_NOSTRIP(x, ...) { if(!(x)) { GEOGL_ERROR_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__)); GEOGL_DEBUG_BREAK(); } } (void(0))
+#define GEOGL_ASSERT_NOSTRIP(x, ...) { if(!(x)) { GEOGL_ERROR_NOSTRIP(GEOGL_PREPARE_LOG_ENTRY_NOSTRIP("Assertion Failed: {0}", GEOGL_FORMAT(__VA_ARGS__))); GEOGL_DEBUG_BREAK(); } } (void(0))
 
 #endif //GEOGL_PUBLICLOG_HPP
